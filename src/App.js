a23e74fada35e98cb7d29720c62ef83a6de11882
onnurinoah <ixtys0704@gmail.com>
2025-12-13 06:54:24 +0900

    Restore: Re-enable MainDisplay component after successful debugging

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-// src/App.js 파일 수정
+// src/App.js 파일 최종 복구 버전
 
 import React from 'react';
-// import MainDisplay from './MainDisplay.js'; // 🚨 임시 주석 처리 (원인 격리)
+import MainDisplay from './MainDisplay.js'; // 🚨 주석 해제하여 컴포넌트 불러오기
 import InputPage from './InputPage.js';
 
 function App() {
+  // URL 쿼리 파라미터를 읽어와 모드를 결정
   const queryParams = new URLSearchParams(window.location.search);
   const mode = queryParams.get('mode');
 
   return (
     <div className="App">
-      {/* 🚨 <MainDisplay /> 대신 단순 텍스트를 임시로 렌더링 */}
-      {mode === 'input' ? <InputPage /> : <h1>MainDisplay 로직 실행 전 테스트 성공!</h1>}
+      {/* 🚨 임시 텍스트를 제거하고 MainDisplay 컴포넌트 호출로 복구 */}
+      {mode === 'input' ? <InputPage /> : <MainDisplay />}
     </div>
   );
 }
